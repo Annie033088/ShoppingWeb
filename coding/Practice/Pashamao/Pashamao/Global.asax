0bbcea45d93e0ad6e969804f018f8ce4359d2519
Annie <FengMu9966@gmail.com>
2024-11-15 18:08:54 +0800

    PashaMao 0.0.1
    Signed-off-by: Annie <FengMu9966@gmail.com>

---
+++ coding/Practice/Pashamao/Pashamao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pashamao.MvcApplication" Language="C#" %>
---
+++ coding/Practice/Pashamao/Pashamao/Global.asax.cs
@@ -0,0 +1,43 @@
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Pashamao
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger ();
+
+        protected void Application_Start()
+        {
+            //可以註冊區域 方便管理不同分區
+            //AreaRegistration.RegisterAllAreas ();
+
+            //全局過濾器 在每次請求都會被調用
+            FilterConfig.RegisterGlobalFilters ( GlobalFilters.Filters );
+
+            //註冊路由規則
+            RouteConfig.RegisterRoutes ( RouteTable.Routes );
+
+            //註冊bundle
+            BundleConfig.RegisterBundles ( BundleTable.Bundles );
+            logger.Info ( "Application Start" );
+        }
+
+        protected void Application_Error( Exception error )
+        {
+            logger.Error( error );
+        }
+
+       protected void Application_End()
+        {
+            logger.Info ( "Application End");
+        }
+
+    }
+}
